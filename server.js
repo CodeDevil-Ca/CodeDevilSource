1c2a715d141f282ff0b85cd8c13269a0f39ebbe5
CodeDevil-Ca <CodeDevil.info@gmail.com>
2020-09-17 11:47:59 -0400

    added more pages

--- server.js
+++ server.js
@@ -11,8 +11,22 @@ app.use((req, res,next)=>{
     res.status(404).sendFile(path.join(__dirname,'public/404.html'));
  });
 
-
 app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname,'public/index.html'));
+});
+app.get('/projects',(req,res) => {
+    res.sendFile(path.join(__dirname,'public/projects.html'));
+});
+app.get('/contact',(req,res) => {
+    res.sendFile(path.join(__dirname,'public/contact.html'));
+});
+app.get('/opinions',(req,res) => {
+    res.sendFile(path.join(__dirname,'public/opinions.html'));
+});
+app.get('/tutorials',(req,res) => {
+    res.sendFile(path.join(__dirname,'public/tutorials.html'));
+});
+app.get('/september20',(req,res) => {
+    res.sendFile(path.join(__dirname,'public/september20.html'));
 });
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
