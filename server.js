2092e9a219ae65cc2bb92ed5da3dc7a93b2f0a33
CodeDevil-Ca <CodeDevil.info@gmail.com>
2020-09-17 12:41:06 -0400

    updated and routed new pages

--- server.js
+++ server.js
@@ -2,14 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 8080;
+const router = express.Router();
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'resources')));
+app.use(express.static(path.join(__dirname,'CSS')));
+app.use(express.static(path.join(__dirname,'JS')));
 
-app.use(express.static('public'));
-app.use(express.static('resources'));
-app.use(express.static('CSS'));
-app.use(express.static('JS'));
-app.use((req, res,next)=>{
-    res.status(404).sendFile(path.join(__dirname,'public/404.html'));
- });
 
 app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname,'public/index.html'));
@@ -29,4 +27,11 @@ app.get('/tutorials',(req,res) => {
 app.get('/september20',(req,res) => {
     res.sendFile(path.join(__dirname,'public/september20.html'));
 });
+app.get('/post1',(req,res)=>{
+    res.sendFile(path.join(__dirname,'public/post1.html'));
+});
+app.use((req, res,next)=>{
+    res.status(404).sendFile(path.join(__dirname,'public/404.html'));
+   
+ });
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
